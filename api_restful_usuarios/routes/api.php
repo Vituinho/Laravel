d31a0d42a306cf684768fd47fcb4b3e3ff5404b9
Vituinho <trabalhovictor531@gmail.com>
2025-12-30 11:49:23 -0300

    refactor: Criando pastas para o controller da Api e Web

--- api_restful_usuarios/routes/api.php
+++ api_restful_usuarios/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\Api\UsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
--- api_restful_usuarios/routes/web.php
+++ api_restful_usuarios/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\UsuarioController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios', function () {
-    return view('usuarios.index');
-});
+Route::get('/usuarios', [UsuarioController::class, 'index']);
