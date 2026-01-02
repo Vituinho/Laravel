8c6486bf6127b5b0fb00782c017a4048734208b4
Vituinho <trabalhovictor531@gmail.com>
2026-01-02 01:30:23 -0300

    feat: Criando rotas da Api e Web

--- api_restful_tarefas/routes/api.php
+++ api_restful_tarefas/routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('tarefas', App\Http\Controllers\Api\TarefaController::class);
