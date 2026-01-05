be40de32bba00fbac686263b399fb53715d1c1d1
Vituinho <trabalhovictor531@gmail.com>
2026-01-05 17:48:00 -0300

    feat: Criando Controller de Usuario, Api e Web (Login/Register)

--- api_restful_eccomerce/app/Http/Controllers/Api/UsuarioController.php
+++ api_restful_eccomerce/app/Http/Controllers/Api/UsuarioController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Usuario;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 
 class UsuarioController extends Controller
 {
@@ -13,14 +15,32 @@ class UsuarioController extends Controller
      */
     public function index()
     {
-        //
+        return User::all();
     }
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(RegisterRequest $request)
     {
-        //
+        $usuario = User::create($request->all());
+        return response()->json($usuario, 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        } else {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
     }
 
     /**
@@ -28,12 +48,12 @@ class UsuarioController extends Controller
      */
     public function show(Usuario $usuario)
     {
-        //
+        return response()->json($usuario);
     }
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Usuario $usuario)
+    public function update(UsuarioRequest $request, Usuario $usuario)
     {
         //
     }
--- api_restful_eccomerce/app/Http/Controllers/Web/UsuarioController.php
+++ api_restful_eccomerce/app/Http/Controllers/Web/UsuarioController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Models\Usuario;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,21 +11,17 @@ class UsuarioController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+
+    public function login()
     {
         return view('usuarios.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function register()
     {
         return view('usuarios.create');
-    }
-    /**
-     * Show the form for editing the specified resource.
-     */
+    } 
+
     public function edit(Produto $produto)
     {
         return view('usuarios.edit');
