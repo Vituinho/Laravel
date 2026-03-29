33734f76973e8f01977f9b4d4c2be6a915663dae
Vituinho <trabalhovictor531@gmail.com>
2026-03-29 13:10:31 -0700

    feat: Criando store do Controller da API

--- tasks-api-laravel-13/app/Http/Controllers/TaskController.php
+++ tasks-api-laravel-13/app/Http/Controllers/TaskController.php
@@ -21,50 +21,12 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        //
-    }
+        $task = Task::create($request->validated());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Task $task)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Task $task)
-    {
-        //
+        return new TaskResource($task);
     }
 }
